a799430362185372950e9c46adf7a2bd338ccb33
tritran078 <236134229+tritran078@users.noreply.github.com>
2026-05-11 02:16:18 -0700

    fixing some issues with get_logger in testing code, standardized protocols for writing a misison program

--- testing_stuff/testing_stuff/motor_test_3.py
+++ testing_stuff/testing_stuff/motor_test_3.py
@@ -1,4 +1,11 @@
 #testing the motor node with abstraction layer
+# remember the sequence of writing mission code:
+# 1) create motor_node instance
+# 2) create auv controller instance with motor node as argument
+# 3) spin the motor node in a separate thread
+# 4) auv.wait_ready() to start
+# 5) auv is for movement and motor is for logging
+# 6) always have safety measures.
 import rclpy
 from pixhawk_packages.auv_controller_node import AUVControllerNode
 from pixhawk_packages.motor_node_manual import Motor_Node
@@ -14,32 +21,34 @@ def main():
     spin_thread.start()
 
     try:
-        auv.get_logger().info("waiting for motors to be ready")
+        motor.get_logger().info("checking if motors are ready")
         auv.wait_ready()  # this must be here
-        auv.get_logger().info("motors are ready")
+        motor.get_logger().info("motors are ready")
 
+        motor.get_logger().info("moving forward NOW")
         auv.forward(duration = 6.0)
-        auv.get_logger().info("moving forward")
 
-        """auv.strafe_right(duration=3.0)
-        auv.get_logger().info("moving right")
-
+        
+        motor.get_logger().info("moving right NOW")
+        auv.strafe_right(duration=5.0)
+        """
         auv.forward(duration=3.0)
-        auv.get_logger().info("moving forward")
+        motor.get_logger().info("moving forward")
 
         auv.strafe_left(duration=3.0)
-        auv.get_logger().info("moving left")
+        motor.get_logger().info("moving left")
 
         auv.backward(duration=3.0)
-        auv.get_logger().info("moving backward")
+        motor.get_logger().info("moving backward")
 
         auv.strafe_right(duration=3.0)
-        auv.get_logger().info("moving right")
+        motor.get_logger().info("moving right")
 
-        auv.stop()"""
+        auv.stop()
+        """
 
     except KeyboardInterrupt:
-        auv.get_logger().info("program stopped. motors are set to neutral and disarm")
+        motor.get_logger().info("program stopped. motors are set to neutral and disarm")
     finally:
         auv.stop()
         time.sleep(1.0)
